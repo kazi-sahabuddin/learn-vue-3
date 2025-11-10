0ac952f22881aca2620d8294ce4cdce1b7cbcca0
kazi-sahabuddin <kazi.sahabuddin16@gmail.com>
2025-11-10 14:38:01 +0600

    feat: vue programmatic routing

--- src/views/BlogPostsGreeting.vue
+++ src/views/BlogPostsGreeting.vue
@@ -1,35 +1,63 @@
 <template>
-    <div class="blog-posts-greeting">
-        <h1>Welcome to the blog posts page!</h1>
-        <h2>
-            Here you can find all the blog posts that have been published by our
-            authors. Click on any of them to read more.
-        </h2>
-    </div>
+  <div class="blog-posts-greeting">
+    <h1>Welcome to the blog posts page!</h1>
+    <h2>
+      Here you can find all the blog posts that have been published by our authors. Click on any of
+      them to read more.
+    </h2>
+    <button @click="goToRandomBlogPost">show random blog post</button>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import api from '@/apis/blogPosts'
+import { onMounted, ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const blogPosts = ref([])
+
+onMounted(loadBlogPosts)
+
+async function loadBlogPosts() {
+  try {
+    blogPosts.value = await api.findAll()
+  } catch (error) {
+    console.log('faield to fetch Blog posts', error)
+  }
+}
+
+function goToRandomBlogPost() {
+  if (blogPosts.value.length === 0) {
+    alert('no blog post available.')
+    return
+  }
+  const randomIndex = Math.floor(Math.random() * blogPosts.value.length)
+  const randomBlogPost = blogPosts.value[randomIndex]
+  router.push({ name: 'blogPost', params: { id: randomBlogPost.id } })
+}
+</script>
 
 <style lang="scss" scoped>
 .blog-posts-greeting {
-    padding: 1.5rem;
-    background-color: #f5f5f5;
-    border-radius: 8px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  padding: 1.5rem;
+  background-color: #f5f5f5;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 
-    h1 {
-        margin-bottom: 1rem;
-        font-size: 2rem;
-        font-weight: bold;
-        color: #333;
-        text-align: center;
-    }
+  h1 {
+    margin-bottom: 1rem;
+    font-size: 2rem;
+    font-weight: bold;
+    color: #333;
+    text-align: center;
+  }
 
-    h2 {
-        font-size: 1.8rem;
-        font-weight: 500;
-        color: #555;
-        text-align: center;
-    }
+  h2 {
+    font-size: 1.8rem;
+    font-weight: 500;
+    color: #555;
+    text-align: center;
+  }
 }
 </style>
