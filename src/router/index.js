63bf2cada15acdbd69ed8086df287c4d55885fa9
kazi-sahabuddin <kazi.sahabuddin16@gmail.com>
2025-11-11 18:27:52 +0600

    feat: login page created

--- src/router/index.js
+++ src/router/index.js
@@ -7,20 +7,21 @@ import BlogPost from '@/views/BlogPost.vue'
 import BlogPostsGreeting from '@/views/BlogPostsGreeting.vue'
 import NotFound from '@/views/NotFound.vue'
 import AdsView from '@/views/AdsView.vue'
+import LogIn from '@/views/LogIn.vue'
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', name: 'home', component: Home },
     {
-      path: '/blogPosts',
+      path: '/blog-posts',
       name: 'blogPosts',
       component: BlogPosts,
       redirect: { name: 'blogPostsGreeting' },
       children: [
         { path: '', name: 'blogPostsGreeting', component: BlogPostsGreeting },
         {
-          path: '/blogPosts/:id(\\d+)',
+          path: ':id(\\d+)',
           name: 'blogPost',
           components: {
             default: BlogPost,
@@ -30,12 +31,20 @@ const router = createRouter({
       ],
     },
     { path: '/about', name: 'about', component: About },
+    { path: '/login', name: 'login', component: LogIn },
     {
       path: '/:pathMatch(.*)*',
       name: 'notFound',
       component: NotFound,
     },
   ],
+})
+
+router.beforeEach((to, from) => {
+  console.log(`${from.name} -> ${to.name}`)
+  if (to.name === 'blogPost') {
+    return '/login'
+  }
 })
 
 export default router
